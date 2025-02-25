d30fb75cf0899ec1ee24166e5698a23d5e591349
soudip07 <soudip89ghosh@gmail.com>
2025-02-25 19:24:20 +0530

    first commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,25 @@
+import React,{useEffect, useState} from 'react'
+import { useLoaderData } from 'react-router-dom'
+function Github() {
+   /* const [data,setData]=useState([])
+    useEffect(()=>{
+        fetch('https://api.github.com/users/soudip07')
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data);
+            setData(data);
+        })
+    },[])*/
+    const data = useLoaderData()
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+    Github followers:{data.followers}</div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader=async()=>{
+    const response=await fetch('https://api.github.com/users/soudip07')
+    return response.json()
+}
